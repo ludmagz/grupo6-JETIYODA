5946ac3a12dfbb5b4f7812a13d40392b37ae8b21
mcra3287 <mcra@cin.ufpe.br>
2024-07-29 11:13:05 -0300

    Update obstaculos.py

--- PROJETO_IP/Codigo/obstaculos.py
+++ PROJETO_IP/Codigo/obstaculos.py
@@ -4,6 +4,7 @@ import random
 
 fragmentos = []
 lasers = []
+coracoes = []
 
 # Função que cria os fragmentos de crachá (pontos)
 
@@ -29,3 +30,17 @@ def criar_lasers(altura, largura):
         lasers.append(pygame.Rect(x_lasers, y_lasers, 150, 100))
     
     return lasers
+
+def criar_coracao(altura, largura):
+
+    y_coracao = random.randint(50, altura - 50)
+
+    x = largura
+
+    if len(coracoes) > 0:
+        coracoes.clear()
+    
+    coracoes.append(pygame.Rect(x, y_coracao, 40, 40))
+
+    
+    return coracoes
