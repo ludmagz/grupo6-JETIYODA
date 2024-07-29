777f18f44eb4ba534d58a91a7e755962aaa7d15e
mcra3287 <mcra@cin.ufpe.br>
2024-07-29 11:12:38 -0300

    Update colisoes.py

--- PROJETO_IP/Codigo/colisoes.py
+++ PROJETO_IP/Codigo/colisoes.py
@@ -20,14 +20,32 @@ def colisao_fragmentos(fragmentos, tela, x, y, fragmentos_cracha, pontos):
 def colisao_laser(lasers, tela, x, y, foguinho, game_over, vidas):
 
     for laser in lasers[:]:
-            laser.x -= 5
-            tela.blit(foguinho, laser.topleft)
-            if laser.colliderect(pygame.Rect(x, y, 104, 124)):
-                lasers.remove(laser)
-                vidas -= 0.5
-                if vidas == 0:
-                    game_over = True
-            if laser.right < 0:
-                lasers.remove(laser)
+        laser.x -= 5
+        tela.blit(foguinho, laser.topleft)
+        if laser.colliderect(pygame.Rect(x, y, 104, 124)):
+            lasers.remove(laser)
+            vidas -= 0.5
+            if vidas == 0:
+                game_over = True
+        if laser.right < 0:
+            lasers.remove(laser)
     
     return lasers, game_over, vidas
+
+def colisao_coracao(coracoes, tela, x, y, vida_imagem, vidas):
+     
+    for coracao in coracoes[:]:
+        coracao.x -= 5
+        tela.blit(vida_imagem, coracao.topleft)
+        if coracao.colliderect(pygame.Rect(x, y, 104, 124)):
+            coracoes.remove(coracao)
+            if vidas < 3:
+                vidas += 1
+            else:
+                vidas = 3
+        if coracao.right < 0:
+            coracoes.remove(coracao)
+    
+    return coracoes, vidas
+    
+        
